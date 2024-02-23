dd8ba4a2ac277d15d89aa421ce3f9df50fcf36ce
Ru Chern Chong <hello@ruchern.xyz>
2024-02-23 23:54:12 +0800

    Add Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lighthearted-phoenix-fd5541.netlify.app/",
+  integrations: [sitemap()],
+});
